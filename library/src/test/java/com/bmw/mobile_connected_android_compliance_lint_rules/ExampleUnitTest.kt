c6a843904dd919284cf25ffe1b6eebeff0c2ff8c
Suzhan Yu <suzhanyu@FreeBSD.local>
2024-03-13 15:24:59 +0800

    init commit

---
+++ library/src/test/java/com/bmw/mobile_connected_android_compliance_lint_rules/ExampleUnitTest.kt
@@ -0,0 +1,18 @@
+package com.bmw.mobile_connected_android_compliance_lint_rules
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        val a = "lint"
+        assertEquals(4, 2 + 2)
+    }
+}
